eef0947caa1e4aa2ddd9e5a257d6287bcb36af3a
muhammad naufal <muhammadnaufal321@students.amikom.ac.id>
2023-04-03 22:17:27 +0700

    pembagian class

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace minggu4
+{
+    class program
+    {
+        static void Main(string[] arg)
+        {
+            mahasiswa mahasiswa1 = new minggu4.mahasiswa();
+            mahasiswa1.nama = "muhammad naufal ihza";
+            mahasiswa1.nim = "22.11.4868";
+            mahasiswa1.kelas = "if 05";
+            mahasiswa1.jurusan = "informatika";
+            mahasiswa1.ipk = 7f;
+            mahasiswa1.infomahasiswa();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ mahasiswa.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace minggu4
+{
+    internal class mahasiswa
+    {
+        private string _nama;
+        private string _nim;
+        private string _kelas;
+        private string _jurusan;
+        private float _ipk;
+
+        public string nama
+        {
+            get
+            { return _nama;}
+            set
+            { _nama = value;}
+        }
+
+        public string nim
+        {
+            get
+            { return _nim;}
+            set
+            { _nim = value; }
+        }
+
+        public string kelas
+        {
+            get { return _kelas;}
+            set { _kelas = value;}    
+        }
+
+        public string jurusan
+        {
+            get { return _jurusan; }
+            set { _jurusan = value;}
+        }
+
+       public float ipk
+        {
+            get { return _ipk;}
+            set { _ipk = value;}
+        }
+
+
+        public void infomahasiswa()
+        {
+            Console.WriteLine(".......... profil mahasiswa ...........");
+            Console.WriteLine("nama mahasiswa : {0}", nama);
+            Console.WriteLine("nim mahasiswa : {0}", nim);
+            Console.WriteLine("kelas mahasiswa : {0}", kelas);
+            Console.WriteLine("jurusan mahasiswa : {0}", jurusan);
+            Console.WriteLine("nilai mahasiswa : {0}", ipk);
+
+            if (ipk < 3)
+            {
+                Console.WriteLine("(ipk mu rendah)");
+            }
+            else if (ipk <= 4)
+            {
+                Console.WriteLine("(ipk gege!)");
+            }
+            else
+            {
+                Console.WriteLine("(hmmmm... ipkmu di luar nalar)");
+            }
+        }
+
+    
+    }
+}
